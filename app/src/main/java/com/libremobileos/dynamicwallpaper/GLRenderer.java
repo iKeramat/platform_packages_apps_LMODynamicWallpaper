dde4db595dad4922aad8b9aa3e1dfe3604404ac4
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-03-27 20:09:15 +0330

    LMODynamicWallpaper: Import project

---
+++ app/src/main/java/com/libremobileos/dynamicwallpaper/GLRenderer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2025 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.dynamicwallpaper;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+}
---
+++ app/src/main/java/com/libremobileos/dynamicwallpaper/GLWallpaperService.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2025 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.dynamicwallpaper;
+
+import android.service.wallpaper.WallpaperService;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.SurfaceHolder;
+
+import com.libremobileos.dynamicwallpaper.R;
+
+import java.util.Calendar;
+
+public class GLWallpaperService extends WallpaperService {
+
+    @Override
+    public Engine onCreateEngine() {
+        return new GLEngine();
+    }
+
+    private class GLEngine extends Engine {
+        private Handler handler = new Handler(Looper.getMainLooper());
+        private Bitmap currentWallpaper, nextWallpaper;
+        private float alpha = 0f; // Transition progress (0 = old wallpaper, 1 = new wallpaper)
+        private Paint paint = new Paint();
+
+        // Define time periods
+        private static final int MORNING_START = 6;
+        private static final int NOON_START = 12;
+        private static final int AFTERNOON_START = 15;
+        private static final int NIGHT_START = 18;
+
+        @Override
+        public void onSurfaceCreated(SurfaceHolder holder) {
+            super.onSurfaceCreated(holder);
+            setWallpaperBasedOnTime();
+            startWallpaperUpdate();
+        }
+
+        private void setWallpaperBasedOnTime() {
+            // Get the current time of day
+            Calendar calendar = Calendar.getInstance();
+            int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+            // Determine the next wallpaper
+            int nextWallpaperRes;
+            if (hourOfDay >= MORNING_START && hourOfDay < NOON_START) {
+                nextWallpaperRes = R.drawable.morning;
+            } else if (hourOfDay >= NOON_START && hourOfDay < AFTERNOON_START) {
+                nextWallpaperRes = R.drawable.noon;
+            } else if (hourOfDay >= AFTERNOON_START && hourOfDay < NIGHT_START) {
+                nextWallpaperRes = R.drawable.afternoon;
+            } else {
+                nextWallpaperRes = R.drawable.night;
+            }
+
+            if (nextWallpaper == null || nextWallpaperRes != getCurrentWallpaperResId()) {
+                nextWallpaper = BitmapFactory.decodeResource(getResources(), nextWallpaperRes);
+                startCrossFadeAnimation();
+            }
+        }
+
+        private int getCurrentWallpaperResId() {
+            if (currentWallpaper == null) return -1;
+            if (currentWallpaper.sameAs(BitmapFactory.decodeResource(getResources(), R.drawable.morning)))
+                return R.drawable.morning;
+            if (currentWallpaper.sameAs(BitmapFactory.decodeResource(getResources(), R.drawable.noon)))
+                return R.drawable.noon;
+            if (currentWallpaper.sameAs(BitmapFactory.decodeResource(getResources(), R.drawable.afternoon)))
+                return R.drawable.afternoon;
+            return R.drawable.night;
+        }
+
+        private void startCrossFadeAnimation() {
+            alpha = 0f;
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    alpha += 0.2f; // Faster transition
+                    if (alpha >= 1f) {
+                        alpha = 1f;
+                        currentWallpaper = nextWallpaper; // Finish transition
+                    } else {
+                        handler.postDelayed(this, 1000 / 60); // 60 FPS smooth animation
+                    }
+                    drawWallpaper();
+                }
+            });
+        }
+
+        private void drawWallpaper() {
+            SurfaceHolder holder = getSurfaceHolder();
+            Canvas canvas = holder.lockCanvas();
+            if (canvas != null) {
+                if (currentWallpaper != null) {
+                    canvas.drawBitmap(currentWallpaper, 0, 0, null);
+                }
+                if (nextWallpaper != null) {
+                    paint.setAlpha((int) (alpha * 255)); // Set alpha for smooth transition
+                    canvas.drawBitmap(nextWallpaper, 0, 0, paint);
+                }
+                holder.unlockCanvasAndPost(canvas);
+            }
+        }
+
+        private void startWallpaperUpdate() {
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    setWallpaperBasedOnTime();
+                    startWallpaperUpdate();
+                }
+            }, 60000); // Check time every minute
+        }
+    }
+}
